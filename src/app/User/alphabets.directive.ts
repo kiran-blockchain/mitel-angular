301251937025a6e3487b8fc2911b1423f0ef787f
kiran.paturi <kiran.paturi@brugu.io>
2021-03-25 11:07:01 +0530

    Added code

--- src/app/User/alphabets.directive.ts
+++ src/app/User/alphabets.directive.ts
@@ -2,18 +2,18 @@ import { Directive, ElementRef,HostListener } from '@angular/core';
 
 
 @Directive({
-  selector: '[alphabetsOnly]'
+  selector: '[allowPattern]'
 })
 export class AlphabetsDirective {
 
   constructor(private ele:ElementRef) {
       
    }
-
    @HostListener('keypress',['$event'])
    onKeyPress(e:any){
         console.log(e); 
-        var regex = /^[a-zA-Z ]*$/; 
+        console.log(this.ele.nativeElement.getAttribute('pattern'));
+        var regex = new RegExp(this.ele.nativeElement.getAttribute('pattern'));
         if(regex.test(e.key)){
             return true;
         }else{
--- src/app/User/login/login.component.ts
+++ src/app/User/login/login.component.ts
@@ -12,10 +12,13 @@ export class LoginComponent implements OnInit {
     Validators.minLength(10),
     Validators.pattern(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/),
     ]),
-    password:new FormControl('')
+    password:new FormControl(''),
+    age:new FormControl(0)
   })
   constructor() { 
   }
+  alphabets ="^[a-zA-Z ]*$";
+  numbersOnly ="^[0-9]*$";
   get f(){
     return this.loginForm.controls;
   }
